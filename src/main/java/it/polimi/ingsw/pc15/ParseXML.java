9699c933d9497e3805bc7233d682665f263929fc
AndreaMazzeo289 <andrea4.mazzeo@mail.polimi.it>
2017-05-16 15:16:20 +0200

    parser xml

--- src/main/java/it/polimi/ingsw/pc15/ParseXML.java
+++ src/main/java/it/polimi/ingsw/pc15/ParseXML.java
@@ -28,24 +28,36 @@ public class ParseXML {
 			DocumentBuilderFactory documentFactory = DocumentBuilderFactory.newInstance();
 			
 			DocumentBuilder builder = documentFactory.newDocumentBuilder();
-			Document document = builder.parse(new File("/Users/andre/LaboratorioProvaFinale/prova-finale-template/XML/Cards_v1.2.xml"));
+			Document document = builder.parse(new File("/Users/andre/LaboratorioProvaFinale/prova-finale-template/XML/Cards_v1.3.xml"));
 			
 			NodeList carte = document.getElementsByTagName("carta");
 			
 			//System.out.println("Totale delle carte: " + carte.getLength());
-			System.out.println("CARTE VERDI: ");
 			
 			for(int i=0; i<carte.getLength(); i++){
-				Node nodo = carte.item(i);
 				
-				if(nodo.getNodeType() == Node.ELEMENT_NODE) {
-					Element carta = (Element)nodo;
+				Element carta = (Element) carte.item(i);
 					
-					String nome = carta.getElementsByTagName("nome").item(0).getFirstChild().getNodeValue();
-					String colore = carta.getElementsByTagName("colore").item(0).getFirstChild().getNodeValue();
-					
-					if(colore.toUpperCase().equals(coloreCarta.toString())){
-						System.out.println("Nome carta: " + nome);
+				String colore = carta.getElementsByTagName("colore").item(0).getFirstChild().getNodeValue();
+				
+				if(colore.toUpperCase().equals(coloreCarta.toString())) {
+					switch(coloreCarta.toString()){
+						case "VERDE":
+							leggiCartaVerde(carta);
+							break;
+							
+						case "GIALLO":
+							leggiCartaGialla(carta);
+							break;
+							
+						case "BLU":
+							leggiCartaBlu(carta);
+							break;
+							
+						case "VIOLA":
+							leggiCartaViola(carta);
+							break;
+						default: System.out.println("ERRORE NELLA SCRITTURA DEL FILE!");
 					}
 				}
 			}
@@ -55,4 +67,187 @@ public class ParseXML {
 		
 	}
 	
+	public static void leggiCartaVerde (Element carta){
+		String ID = carta.getAttribute("id");
+		String nome = carta.getElementsByTagName("nome").item(0).getFirstChild().getNodeValue();
+		String periodo = carta.getElementsByTagName("periodo").item(0).getFirstChild().getNodeValue();
+		String dado = carta.getElementsByTagName("dadoRaccolto").item(0).getFirstChild().getNodeValue();
+		int periodoInt = Integer.parseInt(periodo);
+		
+		System.out.println("ID: " + ID);
+		System.out.println("Nome:" + nome);
+		System.out.println("Periodo:" + periodoInt);
+		System.out.println("Dado raccolto:" + dado);
+		
+		NodeList listaEffettiImm = carta.getElementsByTagName("effettoImm");
+		
+        for (int j = 0; j < listaEffettiImm.getLength(); ++j) {
+            Element effetto = (Element) listaEffettiImm.item(j);
+            String effettoTipo = effetto.getFirstChild().getNodeValue();
+            System.out.println("Effetto immediato: " +effettoTipo);
+        }
+        
+        NodeList listaEffettiPerm = carta.getElementsByTagName("effettoPerm");
+        
+        for (int j = 0; j < listaEffettiPerm.getLength(); ++j) {
+            Element effetto = (Element) listaEffettiPerm.item(j);
+            String effettoTipo = effetto.getFirstChild().getNodeValue();
+            System.out.println("Effetto permanente: " +effettoTipo);
+        }
+        
+        System.out.println("--------------------------------------------------");
+	}
+	
+	public static void leggiCartaGialla (Element carta){
+		
+		String ID = carta.getAttribute("id");
+		String nome = carta.getElementsByTagName("nome").item(0).getFirstChild().getNodeValue();
+		String periodo = carta.getElementsByTagName("periodo").item(0).getFirstChild().getNodeValue();
+		String dado = carta.getElementsByTagName("dadoProduzione").item(0).getFirstChild().getNodeValue();
+		int periodoInt = Integer.parseInt(periodo);
+		
+		System.out.println("ID: " + ID);
+		System.out.println("Nome:" + nome);
+		System.out.println("Periodo:" + periodoInt);
+		System.out.println("Dado produzione:" + dado);
+		
+		NodeList listaCosti = carta.getElementsByTagName("costo");
+		System.out.println("Lista costi:");
+		int listaCostiInt = listaCosti.getLength();
+		
+        for (int j = 0; j < listaCostiInt; j++) {
+            Element costo = (Element) listaCosti.item(j);
+            String risorsa = costo.getElementsByTagName("risorsa").item(0).getFirstChild().getNodeValue();
+            String quantita = costo.getElementsByTagName("quantita").item(0).getFirstChild().getNodeValue();
+            System.out.println("Risorsa["+j+"]:" +risorsa);
+            System.out.println("Quantita["+j+"]:" +quantita);
+        }
+		
+		NodeList listaEffettiImm = carta.getElementsByTagName("effettoImm");
+		int listaEffettiImmInt = listaEffettiImm.getLength();
+		
+        for (int j = 0; j < listaEffettiImmInt; ++j) {
+            Element effetto = (Element) listaEffettiImm.item(j);
+            String effettoTipo = effetto.getFirstChild().getNodeValue();
+            System.out.println("Effetto immediato: " +effettoTipo);
+        }
+        
+        NodeList listaEffettiPerm = carta.getElementsByTagName("effettoPerm");
+        
+        
+        for (int j = 0; j < listaEffettiPerm.getLength(); ++j) {
+            Element effetto = (Element) listaEffettiPerm.item(j);
+            String effettoTipo = effetto.getFirstChild().getNodeValue();
+            System.out.println("Effetto permanente: " +effettoTipo);
+        }
+        
+        System.out.println("--------------------------------------------------");
+	}
+	
+	public static void leggiCartaBlu (Element carta){
+		
+		String ID = carta.getAttribute("id");
+		String nome = carta.getElementsByTagName("nome").item(0).getFirstChild().getNodeValue();
+		String periodo = carta.getElementsByTagName("periodo").item(0).getFirstChild().getNodeValue();
+		int periodoInt = Integer.parseInt(periodo);
+		
+		System.out.println("ID: " + ID);
+		System.out.println("Nome:" + nome);
+		System.out.println("Periodo:" + periodoInt);
+		
+		NodeList listaCosti = carta.getElementsByTagName("costo");
+		System.out.println("Lista costi:");
+		int listaCostiInt = listaCosti.getLength();
+		
+        for (int j = 0; j < listaCostiInt; j++) {
+            Element costo = (Element) listaCosti.item(j);
+            String risorsa = costo.getElementsByTagName("risorsa").item(0).getFirstChild().getNodeValue();
+            String quantita = costo.getElementsByTagName("quantita").item(0).getFirstChild().getNodeValue();
+            System.out.println("Risorsa["+j+"]:" +risorsa);
+            System.out.println("Quantita["+j+"]:" +quantita);
+        }
+		
+		NodeList listaEffettiImm = carta.getElementsByTagName("effettoImm");
+		int listaEffettiImmInt = listaEffettiImm.getLength();
+		
+        for (int j = 0; j < listaEffettiImmInt; ++j) {
+            Element effetto = (Element) listaEffettiImm.item(j);
+            String effettoTipo = effetto.getFirstChild().getNodeValue();
+            System.out.println("Effetto immediato: " +effettoTipo);
+        }
+        
+        NodeList listaEffettiPerm = carta.getElementsByTagName("effettoPerm");
+        
+        
+        for (int j = 0; j < listaEffettiPerm.getLength(); ++j) {
+            Element effetto = (Element) listaEffettiPerm.item(j);
+            String effettoTipo = effetto.getFirstChild().getNodeValue();
+            System.out.println("Effetto permanente: " +effettoTipo);
+        }
+        
+        System.out.println("--------------------------------------------------");
+	}
+	
+	public static void leggiCartaViola(Element carta) {
+		
+		String ID = carta.getAttribute("id");
+		String nome = carta.getElementsByTagName("nome").item(0).getFirstChild().getNodeValue();
+		String periodo = carta.getElementsByTagName("periodo").item(0).getFirstChild().getNodeValue();
+		int periodoInt = Integer.parseInt(periodo);
+		
+		System.out.println("ID: " + ID);
+		System.out.println("Nome:" + nome);
+		System.out.println("Periodo:" + periodoInt);
+		
+		NodeList listaCosti = carta.getElementsByTagName("costo");
+		System.out.println("Lista costi:");
+		int listaCostiInt = listaCosti.getLength();
+		
+        for (int j = 0; j < listaCostiInt; j++) {
+            Element costo = (Element) listaCosti.item(j);
+            String risorsa = costo.getElementsByTagName("risorsa").item(0).getFirstChild().getNodeValue();
+            String quantita = costo.getElementsByTagName("quantita").item(0).getFirstChild().getNodeValue();
+            System.out.println("Risorsa["+j+"]:" +risorsa);
+            System.out.println("Quantita["+j+"]:" +quantita);
+            try {
+            	String requisito = costo.getElementsByTagName("requisito").item(0).getFirstChild().getNodeValue(); 
+            	System.out.println("Requisito["+j+"]:" +requisito);
+            }catch(NullPointerException e){
+            	
+            }
+           
+        }
+        
+        NodeList listaCostiAlt = carta.getElementsByTagName("costoAlt");
+		System.out.println("Lista costi alternativi:");
+		int listaCostiAltInt = listaCostiAlt.getLength();
+		
+        for (int j = 0; j < listaCostiAltInt; j++) {
+            Element costoAlt = (Element) listaCostiAlt.item(j);
+            String risorsa = costoAlt.getElementsByTagName("risorsa").item(0).getFirstChild().getNodeValue();
+            String quantita = costoAlt.getElementsByTagName("quantita").item(0).getFirstChild().getNodeValue();
+            System.out.println("RisorsaAlt["+j+"]:" +risorsa);
+            System.out.println("QuantitaAlt["+j+"]:" +quantita);
+        }
+		
+		NodeList listaEffettiImm = carta.getElementsByTagName("effettoImm");
+		int listaEffettiImmInt = listaEffettiImm.getLength();
+		
+        for (int j = 0; j < listaEffettiImmInt; ++j) {
+            Element effetto = (Element) listaEffettiImm.item(j);
+            String effettoTipo = effetto.getFirstChild().getNodeValue();
+            System.out.println("Effetto immediato: " +effettoTipo);
+        }
+        
+        NodeList listaEffettiPerm = carta.getElementsByTagName("effettoPerm");
+        
+        
+        for (int j = 0; j < listaEffettiPerm.getLength(); ++j) {
+            Element effetto = (Element) listaEffettiPerm.item(j);
+            String effettoTipo = effetto.getFirstChild().getNodeValue();
+            System.out.println("Effetto permanente: " +effettoTipo);
+        }
+        
+        System.out.println("--------------------------------------------------");
+	}
 }
--- src/main/java/it/polimi/ingsw/pc15/Torre.java
+++ src/main/java/it/polimi/ingsw/pc15/Torre.java
@@ -7,10 +7,10 @@ public class Torre {
 	public Torre (SetRisorse spazio1, SetRisorse spazio2, SetRisorse spazio3, SetRisorse spazio4) {
 		
 		this.spazioTorre = new SpazioTorre[4];
-		this.spazioTorre[1]= new SpazioTorre(1,spazio1);
+		/*this.spazioTorre[1]= new SpazioTorre(1,spazio1);
 		this.spazioTorre[2]= new SpazioTorre(3,spazio2);
 		this.spazioTorre[3]= new SpazioTorre(5,spazio3);
-		this.spazioTorre[4]= new SpazioTorre(7,spazio4);
+		this.spazioTorre[4]= new SpazioTorre(7,spazio4);*/
 	}
 	
 	public SpazioTorre[] getSpazioTorre() {
