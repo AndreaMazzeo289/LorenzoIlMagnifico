37e5bedd17577ff9b1808c2e327f64159fca0918
AndreaMaffe <andrea1.mafessoni@mail.polimi.it>
2017-05-22 15:24:52 +0200

    model

--- src/main/java/it/polimi/ingsw/pc15/Model.java
+++ src/main/java/it/polimi/ingsw/pc15/Model.java
@@ -14,11 +14,7 @@ public class Model extends Observable {
 	private Set<Personaggio> setCartePersonaggio;
 	private Set<Edificio> setCarteEdificio;
 	private Set<Impresa> setCarteImpresa;
-	
-	//numero carte
-	private static final int NUMCARTE = 24;
-	
-	
+
 	public Model(){
 		
 		this.plancia = new Plancia(3);
@@ -32,25 +28,25 @@ public class Model extends Observable {
 		
 		// Istanziazione carte territorio :
 		//----------------------------------	
-		for(int i = 0; i<NUMCARTE; i++){
+		for(int i = 0; i<ParseXML.leggiValore("numeroCarteVerdi"); i++){
 			setCarteTerritorio.add((Territorio) parseXML.getCartaXML(ColoreCarta.VERDE));	
 		}
 		
 		// Istanziazione carte personaggio :
 		//----------------------------------
-		for(int i = 0; i<NUMCARTE; i++){
+		for(int i = 0; i<ParseXML.leggiValore("numeroCarteBlu"); i++){
 			setCartePersonaggio.add((Personaggio) parseXML.getCartaXML(ColoreCarta.BLU));
 		}
 		
 		// Istanziazione carte edicio :
 		//----------------------------------
-		for(int i = 0; i<NUMCARTE; i++){
+		for(int i = 0; i<ParseXML.leggiValore("numeroCarteGialle"); i++){
 			setCarteEdificio.add((Edificio) parseXML.getCartaXML(ColoreCarta.GIALLO));
 		}
 		
 		// Istanziazione carte impresa :
 		//----------------------------------
-		for (int i = 0; i<NUMCARTE; i++){
+		for (int i = 0; i<ParseXML.leggiValore("numeroCarteViola"); i++){
 			setCarteImpresa.add((Impresa) parseXML.getCartaXML(ColoreCarta.VIOLA));
 		}
 		
