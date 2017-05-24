c0bd592c70b62d18ce8e3978cdb038cdf6e94da1
AndreaMaffe <andrea1.mafessoni@mail.polimi.it>
2017-05-24 13:27:14 +0200

    setRisorse

--- src/main/java/it/polimi/ingsw/pc15/risorse/SetRisorse.java
+++ src/main/java/it/polimi/ingsw/pc15/risorse/SetRisorse.java
@@ -1,25 +1,20 @@
 package it.polimi.ingsw.pc15.risorse;
 
-
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Map;
 
-
 public class SetRisorse {  
 
-	HashMap<TipoRisorsa,Risorsa> risorse;
+	HashMap<TipoRisorsa, Risorsa> risorse;
 	
 	public SetRisorse (HashSet<Risorsa> risorse) {
 		
 		this.risorse = new HashMap<TipoRisorsa, Risorsa>();
-		
-		Iterator<Risorsa> risorsa = risorse.iterator();	                          //   aggiungo le risorse del set ricevuto come parametro		
-		while(risorsa.hasNext()){		                                          //       nella mappa 
-			Risorsa risorsaExt = risorsa.next();
-			this.risorse.put(risorsaExt.getTipoRisorsa(), risorsaExt);
-		}
+
+		for (Risorsa risorsa : risorse)
+			this.risorse.put(risorsa.getTipoRisorsa(), risorsa);
 
 	}
 	
@@ -31,21 +26,6 @@ public class SetRisorse {
 					risorsa1.getValue().aggiungi(risorsa2.getValue().getQuantità());
 			}
 		}
-		
-		/*Iterator<Map.Entry<TipoRisorsa, Risorsa>> it1 = this.risorse.entrySet().iterator();
-		
-		while(it1.hasNext()){
-			
-			Iterator<Map.Entry<TipoRisorsa, Risorsa>> it2 = setRisorse.getRisorse().entrySet().iterator();
-			
-			while(it2.hasNext()){
-				
-				if(it1.next().getKey().equals(it2.next().getKey())) {
-					
-					it1.next().getValue().aggiungi(it2.next().getValue().getQuantità());
-				}
-			}
-		}*/
 	}
 	
 	public void sottrai (SetRisorse setRisorse) {  //aggiunge al setRisorse corrente le risorse del setRisorse passato come parametro
@@ -56,21 +36,6 @@ public class SetRisorse {
 					risorsa1.getValue().aggiungi(-risorsa2.getValue().getQuantità());
 			}
 		}
-		
-		/*Iterator<Map.Entry<TipoRisorsa, Risorsa>> it1 = this.risorse.entrySet().iterator();
-				
-		while(it1.hasNext()){
-					
-			Iterator<Map.Entry<TipoRisorsa, Risorsa>> it2 = setRisorse.getRisorse().entrySet().iterator();
-					
-			while(it2.hasNext()){
-						
-				if(it1.next().getKey().equals(it2.next().getKey())) {
-							
-					it1.next().getValue().aggiungi(-it2.next().getValue().getQuantità());
-				}
-			}
-		}*/
 		
 	}
 
@@ -88,41 +53,16 @@ public class SetRisorse {
 		}
 		
 		return risultato;
-		
-
-		/*
-		
-		Iterator<Map.Entry<TipoRisorsa, Risorsa>> it1 = this.risorse.entrySet().iterator();
-		
-		while(it1.hasNext()){
-					
-			Iterator<Map.Entry<TipoRisorsa, Risorsa>> it2 = setRisorse.getRisorse().entrySet().iterator();
-					
-			while(it2.hasNext()){
-						
-				if(it1.next().getKey().equals(it2.next().getKey())) {
-							
-					if (it1.next().getValue().paragona(it2.next().getValue().getQuantità()));
-					else return false;
-				}
-			}
-		}
-		return true;
-		
-		
-		*/
 	}
 	
 	public Risorsa getRisorsa(TipoRisorsa tipoRisorsa){
-		//Risorsa prova = risorse.get(tipoRisorsa);
-		//System.out.println(prova);
-		return risorse.get(tipoRisorsa);
 		
+		return risorse.get(tipoRisorsa);	
 	}
+	
 	public HashMap<TipoRisorsa,Risorsa> getRisorse(){
-		
+	
 		return this.risorse;
-		
 	}
 
 }
