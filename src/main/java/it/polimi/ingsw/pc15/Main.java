1eb7d7fdcfb1c84b48afc66e431fc2e15136a5e3
AndreaMazzeo289 <andrea4.mazzeo@mail.polimi.it>
2017-05-25 19:12:56 +0200

    modifiche main

--- src/main/java/it/polimi/ingsw/pc15/Main.java
+++ src/main/java/it/polimi/ingsw/pc15/Main.java
@@ -1,6 +1,7 @@
 package it.polimi.ingsw.pc15;
 
 import java.io.IOException;
+import java.util.Random;
 import java.util.Scanner;
 
 import it.polimi.ingsw.pc15.carte.ColoreCarta;
@@ -58,15 +59,107 @@ public class Main {
 		 *  - attenzione all'effetto della badessa (carta blu) oltre ad aggiunta risorse ha azione su qualsiasi carta
 		 */
 		
+		Random random = new Random();
+		//***********************************************************************************************//
+		int colore1 = random.nextInt(3)+1;
+		int colore2 = random.nextInt(3)+1;
+		int colore3 = random.nextInt(3)+1;
+		int colore4 = random.nextInt(3)+1;
 		
+		System.out.println("colore1: "+colore1);
+		System.out.println("colore2: "+colore2);
+		System.out.println("colore3: "+colore3);
+		System.out.println("colore4: "+colore4);
 		
-		System.out.println("Il giocatore vuole prendere questa carta BLU con il familiare nero: "+ gioco.getPlancia().getSpazioTorre(ColoreCarta.BLU,0).getCarta().getNome());
+		ColoreCarta coloreCarta1=null;
+		ColoreCarta coloreCarta2=null;
+		ColoreCarta coloreCarta3=null;
+		ColoreCarta coloreCarta4=null;
+		
+		switch(colore1){
+		case 1: coloreCarta1 = ColoreCarta.VERDE;
+			break;
+		case 2: coloreCarta1 = ColoreCarta.BLU;
+			break;
+		case 3: coloreCarta1 = ColoreCarta.GIALLO;
+			break;
+		case 4: coloreCarta1 = ColoreCarta.VIOLA;
+			break;
+		}
+		
+		switch(colore2){
+		case 1: coloreCarta2 = ColoreCarta.VERDE;
+			break;
+		case 2: coloreCarta2 = ColoreCarta.BLU;
+			break;
+		case 3: coloreCarta2 = ColoreCarta.GIALLO;
+			break;
+		case 4: coloreCarta2 = ColoreCarta.VIOLA;
+			break;
+		}
+		
+		switch(colore3){
+		case 1: coloreCarta3 = ColoreCarta.VERDE;
+			break;
+		case 2: coloreCarta3 = ColoreCarta.BLU;
+			break;
+		case 3: coloreCarta3 = ColoreCarta.GIALLO;
+			break;
+		case 4: coloreCarta3 = ColoreCarta.VIOLA;
+			break;
+		}
+		
+		switch(colore4){
+		case 1: coloreCarta4 = ColoreCarta.VERDE;
+			break;
+		case 2: coloreCarta4 = ColoreCarta.BLU;
+			break;
+		case 3: coloreCarta4 = ColoreCarta.GIALLO;
+			break;
+		case 4: coloreCarta4 = ColoreCarta.VIOLA;
+			break;
+		}
+		
+		int numeroSpazio1 = random.nextInt(3);
+		int numeroSpazio2 = random.nextInt(3);
+		int numeroSpazio3 = random.nextInt(3);
+		int numeroSpazio4 = random.nextInt(3);
+		
+		System.out.println("la carta numero 1 sta nello spazio numero "+ numeroSpazio1 +" della torre di colore "+coloreCarta1);
+		System.out.println("la carta numero 2 sta nello spazio numero "+ numeroSpazio2 +" della torre di colore "+coloreCarta2);
+		System.out.println("la carta numero 3 sta nello spazio numero "+ numeroSpazio3 +" della torre di colore "+coloreCarta3);
+		System.out.println("la carta numero 4 sta nello spazio numero "+ numeroSpazio4 +" della torre di colore "+coloreCarta4);
+		
+		Spazio spazioNum1 = (Spazio)gioco.getPlancia().getSpazioTorre(coloreCarta1, numeroSpazio1);
+		Spazio spazioNum2 = (Spazio)gioco.getPlancia().getSpazioTorre(coloreCarta2, numeroSpazio2);
+		Spazio spazioNum3 = (Spazio)gioco.getPlancia().getSpazioTorre(coloreCarta3, numeroSpazio3);
+		Spazio spazioNum4 = (Spazio)gioco.getPlancia().getSpazioTorre(coloreCarta4, numeroSpazio4);
+		
+		System.out.println("Il giocatore vuole prendere questa carta con il familiare nero: "+ gioco.getPlancia().getSpazioTorre(coloreCarta1,numeroSpazio1).getCarta().getNome());
+		System.out.println("il dado del familiare nero è: "+familiareNero.getValore());
+		player.occupaSpazio(spazioNum1, familiareNero);
+		
+		System.out.println("Il giocatore vuole prendere questa carta con il familiare bianco: "+ gioco.getPlancia().getSpazioTorre(coloreCarta2,numeroSpazio2).getCarta().getNome());
+		System.out.println("il dado del familiare giallo è: "+familiareBianco.getValore());
+		player.occupaSpazio(spazioNum2, familiareBianco);
+		
+		System.out.println("Il giocatore vuole prendere questa carta con il familiare arancione: "+ gioco.getPlancia().getSpazioTorre(coloreCarta3,numeroSpazio3).getCarta().getNome());
+		System.out.println("il dado del familiare arancione è: "+familiareArancione.getValore());
+		player.occupaSpazio(spazioNum3, familiareArancione);
+		
+		System.out.println("Il giocatore vuole prendere questa carta con il familiare neutro: "+ gioco.getPlancia().getSpazioTorre(coloreCarta4,numeroSpazio4).getCarta().getNome());
+		System.out.println("il dado del familiare neutro è: "+familiareNeutro.getValore());
+		player.occupaSpazio(spazioNum4, familiareNeutro);
+		
+		//***********************************************************************************************//
+		
+		/*System.out.println("Il giocatore vuole prendere questa carta BLU con il familiare nero: "+ gioco.getPlancia().getSpazioTorre(ColoreCarta.BLU,0).getCarta().getNome());
 		System.out.println("il dado del familiare nero è: "+familiareNero.getValore());
 		player.occupaSpazio(spazio1, familiareNero);
 		
 		System.out.println("Il giocatore vuole prendere questa carta BLU con il familiare bianco: "+ gioco.getPlancia().getSpazioTorre(ColoreCarta.BLU,1).getCarta().getNome());
 		System.out.println("il dado del familiare giallo è: "+familiareBianco.getValore());
-		player.occupaSpazio(spazio2, familiareBianco);
+		player.occupaSpazio(spazio2, familiareBianco);*/
 		
 		
 		
