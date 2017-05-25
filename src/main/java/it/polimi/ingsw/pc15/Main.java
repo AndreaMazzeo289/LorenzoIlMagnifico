44d97b6b2b5f1fb8b054e0628ba7a6d8e0d291d7
AndreaMaffe <andrea1.mafessoni@mail.polimi.it>
2017-05-25 11:52:04 +0200

    modif

--- src/main/java/it/polimi/ingsw/pc15/Main.java
+++ src/main/java/it/polimi/ingsw/pc15/Main.java
@@ -19,18 +19,10 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		
-		//Player[] players = new Player[numeroGiocatori];
-		//Thread[] threads = new Thread[numeroGiocatori];
-
-		
-		Player player = new Player ("Maffe", null );
-		
 		Model gioco = new Model(4);
 		View view = new View (gioco);
 		Controller controller = new Controller (gioco, view);
 		gioco.iniziaPartita();
-		
-		Scanner in = new Scanner(System.in);
 		
 		//((Player) gioco.getPlayers().get(0)).occupaSpazio ((Spazio) gioco.getPlancia().getSpaziMercato().get(0), ((Player) gioco.getPlayers().get(0)).getFamiliare(ColoreFamiliare.ARANCIONE));
 		
