c3826c913f9a5f65ac4554afd754cf1d32f5dcac
AndreaMaffe <andrea1.mafessoni@mail.polimi.it>
2017-05-27 16:05:26 +0200

    effetti

--- src/main/java/it/polimi/ingsw/pc15/effetti/FissaValoreFamiliareAScelta.java
+++ src/main/java/it/polimi/ingsw/pc15/effetti/FissaValoreFamiliareAScelta.java
@@ -1,12 +1,39 @@
 package it.polimi.ingsw.pc15.effetti;
 
+import java.util.Scanner;
+
 import it.polimi.ingsw.pc15.player.ColoreFamiliare;
+import it.polimi.ingsw.pc15.player.Familiare;
+import it.polimi.ingsw.pc15.player.Player;
 
 public class FissaValoreFamiliareAScelta extends FissaValoreFamiliare {
 
-	public FissaValoreFamiliareAScelta(ColoreFamiliare colore, int valore) {
-		super(colore, valore);
-		// TODO Auto-generated constructor stub
+	public FissaValoreFamiliareAScelta(int valore) {
+		super(null, valore);
+	}
+	
+	@Override
+	public void attiva(Player player) {
+		System.out.println("Scegli un familiare del quale vuoi fissare il valore a " + Integer.toString(valore) + ":");
+		System.out.println("(1): FAMILIARE NERO; valore corrente = " + player.getFamiliare(ColoreFamiliare.NERO).getValore());
+		System.out.println("(2): FAMILIARE BIANCO; valore corrente = " + player.getFamiliare(ColoreFamiliare.BIANCO).getValore());
+		System.out.println("(3): FAMILIARE ARANCIONE; valore corrente = " + player.getFamiliare(ColoreFamiliare.ARANCIONE).getValore());
+		System.out.println("(4): FAMILIARE NEUTRO; valore corrente = " + player.getFamiliare(ColoreFamiliare.NEUTRO).getValore());
+		
+		Scanner in = new Scanner(System.in);
+		int scelta = in.nextInt();	
+		Familiare familiareScelto;
+		
+		switch (scelta) {
+			case 1: colore = ColoreFamiliare.NERO; break;
+			case 2: colore = ColoreFamiliare.BIANCO; break;
+			case 3: colore = ColoreFamiliare.ARANCIONE; break;
+			case 4: colore = ColoreFamiliare.NEUTRO; break;
+			default: colore = null; break;
+		}
+		
+		fissaValore(player);
+		
 	}
 
 }
