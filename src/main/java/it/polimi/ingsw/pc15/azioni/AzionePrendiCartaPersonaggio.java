123858bbf36dc73d88dbd4276e20a45de85bed8f
AndreaMazzeo289 <andrea4.mazzeo@mail.polimi.it>
2017-05-31 10:52:07 +0200

    azioni

---
+++ src/main/java/it/polimi/ingsw/pc15/azioni/AzionePrendiCartaPersonaggio.java
@@ -0,0 +1,42 @@
+package azioni;
+
+import it.polimi.ingsw.pc15.ParseXML;
+import it.polimi.ingsw.pc15.carte.Carta;
+import it.polimi.ingsw.pc15.carte.ColoreCarta;
+import it.polimi.ingsw.pc15.player.Player;
+
+public class AzionePrendiCartaPersonaggio extends AzionePrendiCarta {
+
+	public AzionePrendiCartaPersonaggio(Player player, Carta carta) {
+		super(player, carta);
+	}
+
+	@Override
+	public boolean èValida() {
+		
+		if (player.getCarte(ColoreCarta.BLU).size() == ParseXML.leggiValore("numeroMaxCarte")) {  
+			System.out.println("Hai raggiunto il limite massimo di carte Personaggio!");
+			return false;
+		}
+		
+		if (!risorseSufficienti()) {
+			System.out.println("Non hai risorse sufficienti per acquistare questa carta!");
+			return false;
+		}
+			
+		return true;
+	}
+
+	@Override
+	public void attiva() {
+
+		if (èValida()) {
+			pagaCosto();
+			daiCarta();
+			carta.attivaEffettoIstantaneo();
+			
+			System.out.println("Il giocatore ha preso la carta BLU: "  + carta.getNome());
+		}
+	}
+
+}
---
+++ src/main/java/it/polimi/ingsw/pc15/azioni/AzionePrendiCartaTerritorio.java
@@ -0,0 +1,53 @@
+package azioni;
+
+import it.polimi.ingsw.pc15.ParseXML;
+import it.polimi.ingsw.pc15.carte.Carta;
+import it.polimi.ingsw.pc15.carte.ColoreCarta;
+import it.polimi.ingsw.pc15.player.Player;
+import it.polimi.ingsw.pc15.risorse.TipoRisorsa;
+
+public class AzionePrendiCartaTerritorio extends AzionePrendiCarta {
+
+	public AzionePrendiCartaTerritorio(Player player, Carta carta) {
+		super(player, carta);
+	}
+	
+	@Override
+	public boolean èValida() {
+		
+		int numeroMaxCarte = ParseXML.leggiValore("numeroMaxCarte");
+		
+		if (player.getCarte(ColoreCarta.VERDE).size() == numeroMaxCarte) {
+			System.out.println("Hai raggiunto il limite massimo di carte Territorio!");
+			return false;
+		}
+		
+		if (player.getEffettiAttivi().requisitoTerritoriAttivo())
+			if (!player.getSetRisorse().getRisorsa(TipoRisorsa.PUNTIMILITARI).paragona(ParseXML.leggiValore("RequisitoMilitareTerritorio" + Integer.toString(player.getCarte(ColoreCarta.VERDE).size()+1)))) {
+				System.out.println("Non hai abbastanza punti militari per ottenere questo Territorio");
+				return false;
+		}
+		
+		if (!risorseSufficienti()) {
+			System.out.println("Non hai risorse sufficienti per acquistare questa carta!");
+			return false;
+		}
+		
+		return true;
+	}
+
+
+	@Override
+	public void attiva() {
+
+		if (èValida()) {
+			pagaCosto();
+			daiCarta();
+			carta.attivaEffettoIstantaneo();
+			
+			System.out.println("Il giocatore ha preso la carta VERDE: "  + carta.getNome());
+		}
+
+	}
+
+}
