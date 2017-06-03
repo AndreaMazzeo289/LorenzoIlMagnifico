de684b1782da172dceaa0e80b2d3f0519fa5f6bb
AndreaMaffe <andrea1.mafessoni@mail.polimi.it>
2017-06-03 16:37:59 +0200

    gui

---
+++ src/main/java/it/polimi/ingsw/pc15/GUI.java
@@ -0,0 +1,40 @@
+package it.polimi.ingsw.pc15;
+
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.print.DocFlavor.URL;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import com.sun.prism.Image;
+
+public class GUI {
+
+	public static void main(String[] args) {
+		
+		JFrame frm = new JFrame("Lorenzo il Magnifico");
+		frm.setVisible(true);
+		frm.setSize(1600, 900);
+		frm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		try {
+			BufferedImage img= ImageIO.read(GUI.class.getResource("/pc15/img/Leaders/leaders_f_c_10.jpg"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		
+		
+		
+		
+
+	}
+
+}
