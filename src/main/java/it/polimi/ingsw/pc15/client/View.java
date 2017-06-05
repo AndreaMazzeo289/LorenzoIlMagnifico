3c206cc6ebbc3d73138e29ca3627508c3f858852
FrancescoGuzzo <francesco.guzzo2@gmail.com>
2017-06-05 15:33:43 +0200

    m

---
+++ src/main/java/it/polimi/ingsw/pc15/client/View.java
@@ -0,0 +1,14 @@
+package it.polimi.ingsw.pc15.client;
+
+import java.util.Observable;
+
+
+public abstract class View extends Observable{
+	
+	public View(ClientController clientController)
+	{
+		
+		this.addObserver(clientController);
+	}
+
+}
