2b62159b2c141ccb8a0ed4a82799390d3b0936fa
AndreaMaffe <andrea1.mafessoni@mail.polimi.it>
2017-06-05 11:11:25 +0200

    Modif

--- src/main/java/it/polimi/ingsw/pc15/GUI.java
+++ src/main/java/it/polimi/ingsw/pc15/GUI.java
@@ -1,40 +1,47 @@
 package it.polimi.ingsw.pc15;
 
-import java.awt.BorderLayout;
-import java.awt.Container;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
 
-import javax.imageio.ImageIO;
-import javax.print.DocFlavor.URL;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-import com.sun.prism.Image;
 
 public class GUI {
 
 	public static void main(String[] args) {
-		
-		JFrame frm = new JFrame("Lorenzo il Magnifico");
-		frm.setVisible(true);
-		frm.setSize(1600, 900);
-		frm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		try {
-			BufferedImage img= ImageIO.read(GUI.class.getResource("lorenzo-magnifico.png"));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		
-		
-		
-		
+	    ImagePanel panel = new ImagePanel(new ImageIcon("img/Leaders/leaders_f_c_01.jpg").getImage());
 
+	    JFrame frame = new JFrame();
+	    frame.getContentPane().add(panel);
+	    frame.pack();
+	    frame.setVisible(true);
+	  }
 	}
+
+	class ImagePanel extends JPanel {
+
+	  private Image img;
+
+	  public ImagePanel(String img) {
+	    this(new ImageIcon(img).getImage());
+	  }
+
+	  public ImagePanel(Image img) {
+	    this.img = img;
+	    Dimension size = new Dimension(img.getWidth(null), img.getHeight(null));
+	    setPreferredSize(size);
+	    setMinimumSize(size);
+	    setMaximumSize(size);
+	    setSize(size);
+	    setLayout(null);
+	  }
+
+	  public void paintComponent(Graphics g) {
+	    g.drawImage(img, 0, 0, null);
+	  }
+
 
 }
