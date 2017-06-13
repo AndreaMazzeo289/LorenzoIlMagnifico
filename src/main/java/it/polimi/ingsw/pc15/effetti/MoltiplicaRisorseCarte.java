5e2948ca4968a91a8a16090663adb47b9f0afb57
AndreaMazzeo289 <andrea4.mazzeo@mail.polimi.it>
2017-06-13 15:43:16 +0200

    leader

--- src/main/java/it/polimi/ingsw/pc15/effetti/MoltiplicaRisorseCarte.java
+++ src/main/java/it/polimi/ingsw/pc15/effetti/MoltiplicaRisorseCarte.java
@@ -5,6 +5,10 @@ import it.polimi.ingsw.pc15.player.Player;
 public class MoltiplicaRisorseCarte extends Effetto {
 	
 	private int moltiplicatore;
+	
+	public MoltiplicaRisorseCarte(int moltiplicatore) {
+		this.moltiplicatore = moltiplicatore;
+	}
 
 	@Override
 	public void attiva(Player player) {
