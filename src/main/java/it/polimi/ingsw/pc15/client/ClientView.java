f15f671353bc33d255dc97f0fc5a2a51fe2658a9
AndreaMaffe <andrea1.mafessoni@mail.polimi.it>
2017-06-14 16:50:49 +0200

    RMI

---
+++ src/main/java/it/polimi/ingsw/pc15/client/ClientView.java
@@ -0,0 +1,21 @@
+package it.polimi.ingsw.pc15.client;
+
+import java.util.Observable;
+import java.util.Observer;
+
+
+public abstract class ClientView extends Observable implements Observer, Runnable {
+	
+	protected ClientModel clientModel;
+	
+	public ClientView(NetworkHandler networkHandler, ClientModel clientModel)
+	{
+		this.clientModel = clientModel;
+		clientModel.addObserver(this);
+		this.addObserver(networkHandler);
+	
+	}
+	
+	public abstract void run();
+
+}
