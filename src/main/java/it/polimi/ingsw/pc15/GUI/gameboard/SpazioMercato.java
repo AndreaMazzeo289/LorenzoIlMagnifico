33349104c9f4dc40664394b2944297a0368e9b49
AndreaMazzeo289 <andrea4.mazzeo@mail.polimi.it>
2017-07-05 20:44:30 +0200

    gui

---
+++ src/main/java/it/polimi/ingsw/pc15/GUI/gameboard/SpazioMercato.java
@@ -0,0 +1,16 @@
+package it.polimi.ingsw.pc15.GUI.gameboard;
+
+import java.awt.Image;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+
+import it.polimi.ingsw.pc15.GUI.GUI;
+
+public abstract class SpazioMercato extends JPanel{
+	
+	public void inserisciFamiliare(String path) {
+	}
+	
+}
